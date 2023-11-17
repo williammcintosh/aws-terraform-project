1befbc082849a54fa65508c1b19675a210d773d9
William McIntosh <46854008+williammcintosh@users.noreply.github.com>
2023-11-16 20:41:48 -0800

    did chapter 6 site doesn't work i fucking hate this

--- modules/data-stores/postgres/main.tf
+++ modules/data-stores/postgres/main.tf
@@ -1,6 +1,18 @@
 provider "aws" {
   region = "us-east-2"
 }
+
+# AWS Secrets Manager
+data "aws_secretsmanager_secret_version" "creds" {
+  secret_id = "db-creds"
+}
+
+locals {
+  #AWS Secrets Manager
+  db_creds = jsondecode(
+    data.aws_secretsmanager_secret_version.creds.secret_string
+  )
+}
 resource "aws_db_instance" "terraform-db" {
   identifier_prefix   = "mcintosh-terraform-db"
   engine              = "postgres"
@@ -9,6 +21,6 @@ resource "aws_db_instance" "terraform-db" {
   skip_final_snapshot = true
 
   db_name             = var.db_name
-  username            = var.db_username
-  password            = var.db_password
+  username            = local.db_creds.username
+  password            = local.db_creds.password
 }
