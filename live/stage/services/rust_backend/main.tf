f417f3a19b9a8b04457e39d5d324516c2a6c84e9
William McIntosh <46854008+williammcintosh@users.noreply.github.com>
2023-12-04 16:47:06 -0800

    got 'hello world' to work

--- live/stage/services/rust_backend/main.tf
+++ live/stage/services/rust_backend/main.tf
@@ -11,22 +11,48 @@ provider "aws" {
 	region = local.region
 }
 
+# terraform {
+# 	# Reminder this is partial config, must use terraform init -backend-config=backend.hcl (just init)
+# 	backend "s3" {
+# 		key = "stage/services/rust_backend/terraform.tfstate"
+# 	}
+# }
+
+# moved to modules/services/ecr-registry
 resource "aws_ecr_repository" "app_ecr_repo" {
 	name = "rust-backend"
 }
 
-# main.tf
-resource "aws_ecs_cluster" "my_cluster" {
-  name = "app-cluster" # Name your cluster here
+resource "aws_ecs_cluster" "rust_backend_cluster" {
+	name = "rust-backend-cluster"
 }
 
-# main.tf
+# Get default VPC for region
+data "aws_vpc" "default" {
+	default = true
+}
+
+# Get default subnet within the aws_vpc
+data "aws_subnets" "default" {
+	filter {
+		name   = "vpc-id"
+		values = [data.aws_vpc.default.id]
+	}
+
+	# Add this filter to select only the subnets in the us-east-2[a-c] Availability Zone because 2d doesn't support t2.micro
+	filter {
+		name   = "availability-zone"
+		values = ["${local.region}a", "${local.region}b", "${local.region}c"]
+	}
+}
+
+# Add fargate serverless resources
 resource "aws_ecs_task_definition" "app_task" {
-  family                   = "app-first-task" # Name your task
-  container_definitions    = <<DEFINITION
+	family                   = "rust-backend-task" # Name your task
+	container_definitions    = <<DEFINITION
   [
     {
-      "name": "app-first-task",
+      "name": "rust-backend-task",
       "image": "${aws_ecr_repository.app_ecr_repo.repository_url}",
       "essential": true,
       "portMappings": [
@@ -40,141 +66,122 @@ resource "aws_ecs_task_definition" "app_task" {
     }
   ]
   DEFINITION
-  requires_compatibilities = ["FARGATE"] # use Fargate as the launch type
-  network_mode             = "awsvpc"    # add the AWS VPN network mode as this is required for Fargate
-  memory                   = 512         # Specify the memory the container requires
-  cpu                      = 256         # Specify the CPU the container requires
-  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+	requires_compatibilities = ["FARGATE"] # use Fargate as the launch type
+	network_mode             = "awsvpc"    # add the AWS VPN network mode as this is required for Fargate
+	memory                   = 512         # Specify the memory the container requires
+	cpu                      = 256         # Specify the CPU the container requires
+	execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
 }
 
-# main.tf
 resource "aws_iam_role" "ecsTaskExecutionRole" {
-  name               = "ecsTaskExecutionRole"
-  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+	name               = "ecsTaskExecutionRole"
+	assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
 
 data "aws_iam_policy_document" "assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
+	statement {
+		actions = ["sts:AssumeRole"]
 
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
+		principals {
+			type        = "Service"
+			identifiers = ["ecs-tasks.amazonaws.com"]
+		}
+	}
 }
 
 resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
-  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+	role       = aws_iam_role.ecsTaskExecutionRole.name
+	policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-# main.tf
-# Provide a reference to your default VPC
-resource "aws_default_vpc" "default_vpc" {
+# Security group to allow ALB listeners to allow incoming reqs on 80 and allow all outgoing (for itself to communicate with VPCs)
+resource "aws_security_group" "alb" {
+	name = "rust-backend-alb"
+}
+resource "aws_security_group_rule" "allow_http_inbound" {
+	type              = "ingress"
+	security_group_id = aws_security_group.alb.id
+	from_port         = local.http_port
+	to_port           = local.http_port
+	protocol          = local.tcp_protocol
+	cidr_blocks       = local.all_ips
 }
 
-# Provide references to your default subnets
-resource "aws_default_subnet" "default_subnet_a" {
-  # Use your own region here but reference to subnet 1a
-  availability_zone = "${local.region}a"
+resource "aws_security_group_rule" "allow_all_outbound" {
+	type              = "egress"
+	security_group_id = aws_security_group.alb.id
+	from_port         = local.any_port
+	to_port           = local.any_port
+	protocol          = local.any_protocol
+	cidr_blocks       = local.all_ips
 }
 
-resource "aws_default_subnet" "default_subnet_b" {
-  # Use your own region here but reference to subnet 1b
-  availability_zone = "${local.region}b"
-}
-
-# main.tf
 resource "aws_alb" "application_load_balancer" {
-  name               = "load-balancer-dev" #load balancer name
-  load_balancer_type = "application"
-  subnets = [ # Referencing the default subnets
-    "${aws_default_subnet.default_subnet_a.id}",
-    "${aws_default_subnet.default_subnet_b.id}"
-  ]
-  # security group
-  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+	name               = "load-balancer-dev" #load balancer name
+	load_balancer_type = "application"
+	subnets = data.aws_subnets.default.ids
+	# security group
+	security_groups = [aws_security_group.alb.id]
 }
 
-# main.tf
-# Create a security group for the load balancer:
-resource "aws_security_group" "load_balancer_security_group" {
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allow traffic in from all sources
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_lb_target_group" "asg" {
+	name     = "rust-backend-asg"
+	port     = local.http_port
+	protocol = "HTTP"
+	target_type = "ip"
+	vpc_id   = data.aws_vpc.default.id
 }
 
-# main.tf
-resource "aws_lb_target_group" "target_group" {
-  name        = "target-group"
-  port        = 80
-  protocol    = "HTTP"
-  target_type = "ip"
-  vpc_id      = "${aws_default_vpc.default_vpc.id}" # default VPC
+# This is what forwards the actual requests to the correct destination behind the load balancer
+resource "aws_lb_listener" "http" {
+	load_balancer_arn = aws_alb.application_load_balancer.arn
+	port              = local.http_port
+	protocol          = "HTTP"
+
+	default_action {
+		type             = "forward"
+		target_group_arn = aws_lb_target_group.asg.arn
+	}
 }
 
-resource "aws_lb_listener" "listener" {
-  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" #  load balancer
-  port              = "80"
-  protocol          = "HTTP"
-  default_action {
-    type             = "forward"
-    target_group_arn = "${aws_lb_target_group.target_group.arn}" # target group
-  }
+resource "aws_ecs_service" "rust_backend_service" {
+	name            = "rust-backend-service"     # Name the service
+	cluster         = aws_ecs_cluster.rust_backend_cluster.id   # Reference the created Cluster
+	task_definition = aws_ecs_task_definition.app_task.arn # Reference the task that the service will spin up
+	launch_type     = "FARGATE"
+	desired_count   = 3 # Set up the number of containers to 3
+
+	load_balancer {
+		target_group_arn = aws_lb_target_group.asg.arn
+		container_name   = aws_ecs_task_definition.app_task.family
+		container_port   = local.http_port # Specify the container port
+	}
+
+	network_configuration {
+		subnets          = data.aws_subnets.default.ids
+		assign_public_ip = true     # Provide the containers with public IPs
+		security_groups  = [aws_security_group.alb.id] # Set up the security group
+	}
 }
 
-# main.tf
-resource "aws_ecs_service" "app_service" {
-  name            = "app-first-service"     # Name the service
-  cluster         = "${aws_ecs_cluster.my_cluster.id}"   # Reference the created Cluster
-  task_definition = "${aws_ecs_task_definition.app_task.arn}" # Reference the task that the service will spin up
-  launch_type     = "FARGATE"
-  desired_count   = 3 # Set up the number of containers to 3
-
-  load_balancer {
-    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Reference the target group
-    container_name   = "${aws_ecs_task_definition.app_task.family}"
-    container_port   = "${local.http_port}" # Specify the container port
-  }
-
-  network_configuration {
-    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}"]
-    assign_public_ip = true     # Provide the containers with public IPs
-    security_groups  = ["${aws_security_group.service_security_group.id}"] # Set up the security group
-  }
-}
-
-# main.tf
+# create security groups that will only allow the traffic from the created load balancer
 resource "aws_security_group" "service_security_group" {
-  ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    # Only allowing traffic in from the load balancer security group
-    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
-  }
+	ingress {
+		from_port = 0
+		to_port   = 0
+		protocol  = "-1"
+		# Only allowing traffic in from the load balancer security group
+		security_groups = [aws_security_group.alb.id]
+	}
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+	egress {
+		from_port   = 0
+		to_port     = 0
+		protocol    = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
 }
 
-# main.tf
-#Log the load balancer app URL
 output "app_url" {
-  value = aws_alb.application_load_balancer.dns_name
+	value = aws_alb.application_load_balancer.dns_name
 }
